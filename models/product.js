457544c59b30785293d706684dd63d21ebc32540
sayanrick <sayanrick100@gmail.com>
2023-12-14 21:57:46 +0530

    Getting Started with Mongo DB

---
+++ models/product.js
@@ -0,0 +1,40 @@
+const getDb = require("../util/database").getDb;
+
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
+  }
+
+  save() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => {
+
+      });
+  }
+}
+
+module.exports = Product;
