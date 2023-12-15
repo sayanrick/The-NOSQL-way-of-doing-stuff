c8a05a0e1e546c8b930705c3294c68b538e176b1
sayanrick <sayanrick100@gmail.com>
2023-12-15 16:59:54 +0530

    Integrating CRUD with MongoDB

--- models/product.js
+++ models/product.js
@@ -1,24 +1,33 @@
+const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
-    this.imageUrl = imageUrl;
     this.description = description;
+    this.imageUrl = imageUrl;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    let dbOp;
+    
+    if (this._id) {
+      // update the product
+      dbOp = db.collection("products").updateOne({ _id: this._id }, { $set: this });
+    } else {
+      // insert a new product
+      dbOp = db.collection("products").insertOne(this);
+    }
+
+    return dbOp.then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }
 
   static fetchAll() {
@@ -31,10 +40,37 @@ class Product {
         console.log(products);
         return products;
       })
-      .catch(err => {
+      .catch((err) => {});
+  }
 
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((err) => {
+        console.log(err);
       });
   }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db.collection('products').deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(result => {
+        console.log('Deleted');
+        return result; // Return the result to handle it in the calling code
+      })
+      .catch(err => {
+        console.log(err);
+        throw err; // Rethrow the error to propagate it to the calling code
+      });
+  }
+  
 }
 
 module.exports = Product;
