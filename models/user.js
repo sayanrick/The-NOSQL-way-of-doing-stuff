d4d26e4528d77893c03ed80a0291fd46fb60f039
sayanrick <sayanrick100@gmail.com>
2023-12-17 16:05:41 +0530

    Deleting Cart Items

--- models/user.js
+++ models/user.js
@@ -43,6 +43,43 @@ class User {
       );
   }
 
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map(i => {
+      return i.productId;
+    });
+  
+    return db.collection('products').find({_id: {$in: productIds}}).toArray()
+      .then(products => {
+        return products.map(p => {
+          return {
+            ...p,
+            quantity: this.cart.items.find(i => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity
+          };
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: {items: updatedCartItems} } }
+      );
+
+  }
+  
+
   static findById(userId) {
     const db = getDb();
     return db
@@ -59,20 +96,3 @@ class User {
 }
 
 module.exports = User;
-
-// const Sequelize = require('sequelize');
-
-// const sequelize = require('../util/database');
-
-// const User = sequelize.define('user', {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true
-//   },
-//   name: Sequelize.STRING,
-//   email: Sequelize.STRING
-// });
-
-// module.exports = User;
